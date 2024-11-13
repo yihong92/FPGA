559f945f0b136b122a8836540ec68e8b56fb6eea
houg <CC8801235@gmail.com>
2024-11-13 18:26:01 +0800

    Update pingpong.vhd

--- HW4/sources_1/new/pingpong.vhd
+++ HW4/sources_1/new/pingpong.vhd
@@ -51,80 +51,43 @@ architecture Behavioral of pingpong is
 	signal score_r : std_logic_vector(3 downto 0);
 	type   state_type is (initial,move_left,move_right,win_l,win_r);
 	signal state   : state_type;
+	signal pre_state : state_type;
+	signal limt    : std_logic_vector(3 downto 0);
 	
 	
-	signal div     : std_logic_vector(60 downto 0);
+	
+	signal div    : std_logic_vector(60 downto 0);
     signal e_clk  : std_logic;
 	
 
 begin
 LED <= ball;
 
+
 fsm : process(i_clk, i_rst, btn_l, btn_r, btn_s)  --i_clk
 begin
 	if i_rst = '1' then
 		state <= initial;
 	elsif rising_edge(i_clk) then
+		pre_state <= state;
 		case state is
 			when initial =>
 				if btn_l = '1' then
 					state <= move_right;
 				end if;
 			when move_right =>
-				if ball = "10000000" then
-					if btn_r = '1' then
-						state <= move_left;
-					else
-						state <= win_l;
-					end if;
-                elsif ball = "00000010" then
-				    if btn_r = '1' then
-				        state <= win_l;
-				    end if;
-				elsif ball = "00000100" then
-				    if btn_r = '1' then
-				        state <= win_l;
-				    end if;
-				elsif ball = "00001000" then
-				    if btn_r = '1' then
-				        state <= win_l;
-				    end if;
-				elsif ball = "00010000" then
-				    if btn_r = '1' then
-				        state <= win_l;
-				    end if;
-				elsif ball = "00100000" then
-				    if btn_r = '1' then
-				        state <= win_l;
-				    end if;
+				if ball = "10000000" and btn_r = '1' then
+					state <= move_left;
+					
+                elsif (ball = "00000000") or (ball > "00000001" and btn_r = '1') then
+				    state <= win_l;
 				end if;
 			when move_left =>
-				if ball = "00000001" then
-					if btn_l = '1' then
-						state <= move_right;
-					else
-						state <= win_r;
-					end if;
-			    elsif ball = "01000000" then
-				    if btn_l = '1' then
-				        state <= win_r;
-				    end if;
-				elsif ball = "00100000" then
-				    if btn_l = '1' then
-				        state <= win_r;
-				    end if;
-				elsif ball = "00010000" then
-				    if btn_l = '1' then
-				        state <= win_r;
-				    end if;
-				elsif ball = "00001000" then
-				    if btn_l = '1' then
-				        state <= win_r;
-				    end if;
-				elsif ball = "00000100" then
-				    if btn_l = '1' then
-				        state <= win_r;
-				    end if;
+				if ball = "00000001" and btn_l = '1' then
+					state <= move_right;
+					
+                elsif (ball = "00000000") or (ball > "10000000" and btn_l = '1') then
+				    state <= win_r;
 				end if;
 			when win_l =>
 				if score_l = "0100" then
@@ -155,17 +118,17 @@ begin
 			when initial =>
 				ball <= "00000001";
 			when move_right =>
-				ball <= ball(6 downto 0) & ball(7);
+				ball <= ball(6 downto 0) & '0';
 			when move_left =>
-				ball <= ball(0) & ball(7 downto 1);
+				ball <= '0' & ball(7 downto 1);
 			when win_l =>
-				if btn_s = '1' then
+				if pre_state = win_l then
 					ball <= score_r & score_l;
 				else
 					ball <= "00000001";
 				end if;
 			when win_r =>
-				if btn_s = '1' then
+				if pre_state = win_r then
 					ball <= score_r & score_l;
 				else
 					ball <= "10000000";
@@ -174,97 +137,36 @@ begin
 	end if;
 end process;
 
-score_left : process(i_clk, i_rst, btn_l, btn_r, btn_s)  --i_clk
+score : process(i_clk, i_rst, btn_l, btn_r, btn_s)  --i_clk
 begin
 	if i_rst = '1' then
 		score_l <= "0000";
-	elsif rising_edge(i_clk) then
-		case state is
-			when initial =>
-                score_l <= "0000";
-			when move_right =>
-				if ball = "10000000" then
-					if btn_r = '1' then
-					    score_l <= score_l;
-					else
-					    score_l <= score_l + '1';
-					end if;
-				elsif ball = "00000010" then
-				    if btn_r = '1' then
-				        score_l <= score_l + '1';
-				    end if;
-				elsif ball = "00000100" then
-				    if btn_r = '1' then
-				        score_l <= score_l + '1';
-				    end if;
-				elsif ball = "00001000" then
-				    if btn_r = '1' then
-				        score_l <= score_l + '1';
-				    end if;
-				elsif ball = "00010000" then
-				    if btn_r = '1' then
-				        score_l <= score_l + '1';
-				    end if;
-				elsif ball = "00100000" then
-				    if btn_r = '1' then
-				        score_l <= score_l + '1';
-				    end if;
-				end if;
-			when move_left =>
-                null;
-			when win_l =>
-			    null;
-			when win_r =>
-                null;
-		end case;
-	end if;
-end process;
-
-score_right : process(i_clk, i_rst, btn_l, btn_r, btn_s)  --i_clk
-begin
-	if i_rst = '1' then
 		score_r <= "0000";
 	elsif rising_edge(i_clk) then
 		case state is
 			when initial =>
-                score_r <= "0000";
+                score_l <= "0000";
+				score_r <= "0000";
 			when move_right =>
-                null;
+				null;
 			when move_left =>
-                if ball = "00000001" then
-					if btn_l = '1' then
-					    score_r <= score_r;
-					else
-					    score_r <= score_r + '1';
-					end if;
-			    elsif ball = "01000000" then
-				    if btn_l = '1' then
-				        score_r <= score_r + '1';
-				    end if;
-				elsif ball = "00100000" then
-				    if btn_l = '1' then
-				        score_r <= score_r + '1';
-				    end if;
-				elsif ball = "00010000" then
-				    if btn_l = '1' then
-				        score_r <= score_r + '1';
-				    end if;
-				elsif ball = "00001000" then
-				    if btn_l = '1' then
-				        score_r <= score_r + '1';
-				    end if;
-				elsif ball = "00000100" then
-				    if btn_l = '1' then
-				        score_r <= score_r + '1';
-				    end if;
-				end if;
-			when win_l =>
-			    null;
-			when win_r =>
                 null;
+			when win_l =>
+			    if pre_state = move_right then
+					score_l <= score_l + '1';
+				else 
+					score_l <= score_l;
+				end if;
+			when win_r =>
+				if pre_state = move_left then
+					score_r <= score_r + '1';
+				else 
+					score_r <= score_r;
+				end if;
 		end case;
 	end if;
 end process;
+
 div_clk : process(i_clk, i_rst)
 begin
     if i_rst = '1' then
@@ -274,4 +176,8 @@ begin
     end if;
 end process;
 e_clk <= div(24);
+
+
+
+
 end Behavioral;
