4c0a2a662f989d424983ed722bc2daf220676d40
houg <CC8801235@gmail.com>
2024-11-20 18:15:49 +0800

    變速球

--- HW4/sources_1/new/pingpong.vhd
+++ HW4/sources_1/new/pingpong.vhd
@@ -1,7 +1,37 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2024/11/08 21:46:20
+-- Design Name: 
+-- Module Name: pingpong - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use IEEE.std_logic_unsigned.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity pingpong is
 port(
@@ -19,18 +49,20 @@ architecture Behavioral of pingpong is
 	signal ball    : std_logic_vector(7 downto 0);
 	signal score_l : std_logic_vector(3 downto 0);
 	signal score_r : std_logic_vector(3 downto 0);
-	type   state_type is (initial,move_left,move_right,win_l,win_r);
+	type   state_type is (initial,move_left,move_right,win_l,win_r,ready_ml,ready_mr);
 	signal state   : state_type;
 	signal pre_state : state_type;
-	
-	signal div        : std_logic_vector(24 downto 0);
-    signal e_clk    : std_logic;
-    
-    signal i : integer range 0 to 30;
+	signal i : integer range 0 to 30;
     signal i2 : integer range 0 to 3;
     signal lfsr : std_logic_vector (1 downto 0);
     signal feedback : std_logic;
     signal v_clk : std_logic;
+
+	
+	
+	
+	signal div    : std_logic_vector(60 downto 0);
+    signal e_clk  : std_logic;
 	
 
 begin
@@ -67,7 +99,7 @@ begin
 					state <= initial;
 				else
 					if btn_l = '1' then
-						state <= move_right;
+						state <= ready_mr;
 					end if;
 				end if;
 			when win_r =>
@@ -75,8 +107,19 @@ begin
 					state <= initial;
 				else
 					if btn_r = '1' then
-						state <= move_left;
+						state <= ready_ml;
 					end if;
+				end if;
+			when ready_mr =>
+				if ball = "00000001" then 
+					state <= move_right;
+				else state <= ready_mr;
+				end if;
+			when ready_ml =>
+				if ball = "10000000" then 
+					state <= move_left;
+				else 
+					state <= ready_ml;
 				end if;
 		end case;
 	end if;
@@ -95,17 +138,13 @@ begin
 			when move_left =>
 				ball <= '0' & ball(7 downto 1);
 			when win_l =>
-				if btn_s = '1' then
-					ball <= score_r & score_l;
-				else
-					ball <= "00000001";
-				end if;
+				ball <= score_r & score_l;
 			when win_r =>
-				if btn_s = '1' then
-					ball <= score_r & score_l;
-				else
-					ball <= "10000000";
-				end if;
+				ball <= score_r & score_l;
+			when ready_mr =>
+				ball <= "00000001";
+			when ready_ml =>
+				ball <= "10000000";
 		end case;
 	end if;
 end process;
@@ -136,6 +175,10 @@ begin
 				else 
 					score_r <= score_r;
 				end if;
+			when ready_mr =>
+				null;
+			when ready_ml =>
+				null;
 		end case;
 	end if;
 end process;
@@ -149,7 +192,6 @@ begin
     end if;
 end process;
 e_clk <= div(24);
-
 process(e_clk, i_rst)
 begin
     if i_rst = '1' then
@@ -164,4 +206,9 @@ begin
     end if;
 end process;
 v_clk <= div(i);
+
+
+
+
+
 end Behavioral;
